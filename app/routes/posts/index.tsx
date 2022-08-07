86b2d0b54a1bd83b5038de3e2205909efdda2ad8
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-08-06 22:58:15 -0400

    pushing up what I have for  now

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,22 +1,19 @@
 import { Link, useLoaderData } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
+import { getPosts } from "~/models/post.server";
 
-export const loader = async () => {
-  const posts = [
-    {
-      slug: "my-first-post",
-      title: "My First Post!",
-    },
-    {
-      slug: "trail-riding-with-onewheel",
-      title: "Trail Riding with Onewheel!",
-    },
-  ];
-  return json({ posts });
+type LoaderData = {
+  posts: Awaited<ReturnType<typeof getPosts>>;
+};
+
+export const loader: LoaderFunction = async () => {
+  const posts = await getPosts();
+  return json<LoaderData>({ posts });
 };
 
 export default function PostsRoute() {
-  const { posts } = useLoaderData();
+  const { posts } = useLoaderData() as LoaderData;
   return (
     <main>
       <h1>Posts</h1>
