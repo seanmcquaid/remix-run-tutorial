cbb45c2f8fa3f97ff9c8a15f910f17c508503304
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-08-11 17:53:09 +0200

    forms with remix

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -1,3 +1,25 @@
+import { Form } from "@remix-run/react";
+import type { ActionFunction } from "@remix-run/server-runtime";
+import { redirect } from "@remix-run/server-runtime";
+import { createPost } from "~/models/post.server";
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const title = formData.get("title");
+
+  await createPost({ title });
+  return redirect("/posts/admin");
+};
+
 export default function NewPostRoute() {
-  return <div>New Post Route</div>;
+  return (
+    <Form method="post">
+      <p>
+        <label>
+          Post Title: <input type="text" name="title" />
+        </label>
+      </p>
+      <button type="submit">Post</button>
+    </Form>
+  );
 }
